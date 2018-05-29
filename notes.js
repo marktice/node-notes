fe3238b302b5eb96952236725f412227f06b8d9a
Mark Tice <marktice@protonmail.com>
2018-05-30 00:20:11 +1000

    Refactor notes.js

--- notes.js
+++ notes.js
@@ -2,35 +2,40 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+const fetchNotes = () => {
+  try {
+    const notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (error) {
+    return []; 
+  }
+};
+
+const saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 const addNote = (title, body) => {
   console.log('Adding note');
-  
-  var notes = [];
-  var note = {
+  const notes = fetchNotes();
+  const note = {
     title,
     body
   };
-  
-  try {
-    var notesString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(notesString);
-  } catch (error) {
-    
-  }
-
-  var duplicateNotes = notes.filter((note) => note.title === title);
+  const duplicateNotes = notes.filter((note) => note.title === title);
 
   if (duplicateNotes.length === 0) {
     notes.push(note);
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    saveNotes(notes);
+    return note;
   }
 };
 
 const getAll = () => {
   console.log('Getting all notes');
-  // my attempt
+
   try {
-    var notesString = fs.readFileSync('notes-data.json');
+    const notesString = fs.readFileSync('notes-data.json');
     notes = JSON.parse(notesString);
     for (const note of notes) {
       console.log('Title', note.title);
